f779753dc9d5d8738380b29bd7b5c9818d4e33be
Eugene <khiznyak.1988@gmail.com>
2023-03-06 21:23:20 +0200

    Added menu to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 // svg icons
-import svgIconWallet from "@/public/btn1.svg";
 import SvgIcons from "./SvgIcons";
 
 
@@ -40,14 +39,152 @@ const Header: React.FC = () => {
               Home
             </a>
           </li>
-          <li className="px-2">
-            <a className="block px-4 py-2">Pages</a>
+          <li className="dropdown dropdown-hover px-2">
+            <label
+              tabIndex={0}
+              className="block px-4 py-2 hover:text-[#8358ff] cursor-pointer"
+            >
+              Pages
+            </label>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu shadow bg-white rounded-box w-[350px] text-[14px]"
+            >
+              <div className="grid grid-cols-2">
+                <ul className="menu menu-compact w-auto rounded-box px-2 py-4">
+                  <li>
+                    <a>Maintenance</a>
+                  </li>
+                  <li>
+                    <a>Single Case Study</a>
+                  </li>
+                  <li>
+                    <a>Item Details</a>
+                  </li>
+                  <li>
+                    <a>Collection</a>
+                  </li>
+                  <li>
+                    <a>Rankings</a>
+                  </li>
+                  <li>
+                    <a>Edit Profile</a>
+                  </li>
+                  <li>
+                    <a>Contact</a>
+                  </li>
+                  <li>
+                    <a>Login</a>
+                  </li>
+                  <li>
+                    <a>Terms of service</a>
+                  </li>
+                </ul>
+                <ul className="menu menu-compact w-auto rounded-box px-2 py-4">
+                  <li>
+                    <a>Case Studies</a>
+                  </li>
+                  <li>
+                    <a>Careers</a>
+                  </li>
+                  <li>
+                    <a>Explore Colections</a>
+                  </li>
+                  <li>
+                    <a>Activity</a>
+                  </li>
+                  <li>
+                    <a>User</a>
+                  </li>
+                  <li>
+                    <a>About</a>
+                  </li>
+                  <li>
+                    <a>Wallet</a>
+                  </li>
+                  <li>
+                    <a>Page404</a>
+                  </li>
+                </ul>
+              </div>
+            </ul>
           </li>
-          <li className="px-2">
-            <a className="block px-4 py-2">Explore</a>
+          <li className="dropdown dropdown-hover px-2">
+            <label
+              tabIndex={1}
+              className="block px-4 py-2 hover:text-[#8358ff] cursor-pointer"
+            >
+              Explore
+            </label>
+            <ul
+              tabIndex={1}
+              className="dropdown-content menu shadow bg-white rounded-box w-[350px] text-[14px]"
+            >
+              <div className="grid grid-cols-2">
+                <ul className="menu menu-compact w-auto rounded-box px-2 py-4">
+                  <li>
+                    <a>All NFTs</a>
+                  </li>
+                  <li>
+                    <a>Photography</a>
+                  </li>
+                  <li>
+                    <a>Art</a>
+                  </li>
+                  <li>
+                    <a>Sports</a>
+                  </li>
+                  <li>
+                    <a>Collectibles</a>
+                  </li>
+                </ul>
+                <ul className="menu menu-compact w-auto rounded-box px-2 py-4">
+                  <li>
+                    <a>Tradind Cards</a>
+                  </li>
+                  <li>
+                    <a>Domain Names</a>
+                  </li>
+                  <li>
+                    <a>Utility</a>
+                  </li>
+                  <li>
+                    <a>Virtual Worlds</a>
+                  </li>
+                </ul>
+              </div>
+            </ul>
           </li>
-          <li className="px-2">
-            <a className="block px-4 py-2">Resources</a>
+          <li className="dropdown dropdown-hover px-2">
+            <label
+              tabIndex={2}
+              className="block px-4 py-2 hover:text-[#8358ff] cursor-pointer"
+            >
+              Resources
+            </label>
+            <ul
+              tabIndex={2}
+              className="dropdown-content menu menu-compact shadow bg-white rounded-box w-52 px-2 py-4"
+            >
+              <li>
+                <a>Help Center</a>
+              </li>
+              <li>
+                <a>Platform Status</a>
+              </li>
+              <li>
+                <a>Partners</a>
+              </li>
+              <li>
+                <a>Blog</a>
+              </li>
+              <li>
+                <a>Single Post</a>
+              </li>
+              <li>
+                <a>Newsletter</a>
+              </li>
+            </ul>
           </li>
           <li className="px-2">
             <a className="block px-4 py-2">Create</a>
@@ -56,8 +193,29 @@ const Header: React.FC = () => {
 
         {/* buttons */}
         <div>
-          <button className="btn btn-circle group bg-white focus:bg-[#8358ff] hover:bg-[#8358ff] border-[#ccc] hover:border-[#8358ff]">
-            <SvgIcons name="wallet" width={24} height={24} className="group-hover:fill-white group-focus:fill-white fill-black" />
+          <button className="btn btn-circle group bg-white focus:bg-[#8358ff] hover:bg-[#8358ff] border-[#ccc] hover:border-[#8358ff] w-12 h-10 mr-2">
+            <SvgIcons
+              name="wallet"
+              width={24}
+              height={24}
+              className="group-hover:fill-white group-focus:fill-white fill-black"
+            />
+          </button>
+          <button className="btn btn-circle group bg-white focus:bg-[#8358ff] hover:bg-[#8358ff] border-[#ccc] hover:border-[#8358ff] w-12 h-10 mr-2">
+            <SvgIcons
+              name="user"
+              width={24}
+              height={24}
+              className="group-hover:fill-white group-focus:fill-white fill-black"
+            />
+          </button>
+          <button className="btn btn-circle group bg-white focus:bg-[#8358ff] hover:bg-[#8358ff] border-[#ccc] hover:border-[#8358ff] w-12 h-10">
+            <SvgIcons
+              name="moon"
+              width={24}
+              height={24}
+              className="group-hover:fill-white group-focus:fill-white fill-black"
+            />
           </button>
         </div>
       </div>
--- components/SvgIcons.tsx
+++ components/SvgIcons.tsx
@@ -8,16 +8,43 @@ interface ISvgOptions {
 }
 
 const SvgIcons: React.FC<ISvgOptions> = ({ name, width, height, className }) => {
-  if (name === 'wallet') {
+  if (name === "wallet") {
     return (
-      <svg width={width} height={height} className= {className} xmlns="http://www.w3.org/2000/svg">
+      <svg
+        width={width}
+        height={height}
+        className={className}
+        xmlns="http://www.w3.org/2000/svg"
+      >
         <path fill="none" d="M0 0h24v24H0z"></path>
         <path d="M22 6h-7a6 6 0 1 0 0 12h7v2a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v2zm-7 2h8v8h-8a4 4 0 1 1 0-8zm0 3v2h3v-2h-3z"></path>
       </svg>
     );
-  }
-  else
-    return null;
+  } else if (name === "user") {
+    return (
+      <svg
+        width={width}
+        height={height}
+        className={className}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path fill="none" d="M0 0h24v24H0z"></path>
+        <path d="M11 14.062V20h2v-5.938c3.946.492 7 3.858 7 7.938H4a8.001 8.001 0 0 1 7-7.938zM12 13c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6z"></path>
+      </svg>
+    );
+  } else if (name === "moon") {
+    return (
+      <svg
+        width={width}
+        height={height}
+        className={className}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path fill="none" d="M0 0h24v24H0z"></path>
+        <path d="M12 18a6 6 0 1 1 0-12 6 6 0 0 1 0 12zM11 1h2v3h-2V1zm0 19h2v3h-2v-3zM3.515 4.929l1.414-1.414L7.05 5.636 5.636 7.05 3.515 4.93zM16.95 18.364l1.414-1.414 2.121 2.121-1.414 1.414-2.121-2.121zm2.121-14.85l1.414 1.415-2.121 2.121-1.414-1.414 2.121-2.121zM5.636 16.95l1.414 1.414-2.121 2.121-1.414-1.414 2.121-2.121zM23 11v2h-3v-2h3zM4 11v2H1v-2h3z"></path>
+      </svg>
+    );
+  } else return null;
 };
 
 export default SvgIcons;
